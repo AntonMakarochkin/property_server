13b895a5b87aef5030a1ad181899756b07e77020
Anton.Makarochkin@evraz.com <Anton.Makarochkin@evraz.com>
2025-02-16 16:52:42 +0300

    front: доработки

--- server/apartments.js
+++ server/apartments.js
@@ -1,8 +1,118 @@
-export default function registration(app, pool) {
-	app.get('/apartments', function (req, res) {
-		pool.query('SELECT * FROM property_users').then((data) => {
-			res.json(data[0]);
+import pkg from 'easy-yandex-s3';
+import multer from 'multer';
+function convertFiltersToSQL(filters) {
+	const sqlParams = [];
+	Object.entries(filters).map(([key, value]) => {
+		if (key === 'residence') {
+			sqlParams.push(`${key} = '${value}'`);
+		}
+		if (key === 'rooms') {
+			const sqlRooms = value.map((room) => `rooms = "${room}"`).join(' OR ');
+			sqlParams.push(sqlRooms);
+		}
+		if (key === 'priceFrom') {
+			sqlParams.push(`price > ${value}`);
+		}
+		if (key === 'priceTo') {
+			sqlParams.push(`price < ${value}`);
+		}
+		if (key === 'metersFrom') {
+			sqlParams.push(`m2 > '${value}'`);
+		}
+		if (key === 'metersTo') {
+			sqlParams.push(`m2 < '${value}'`);
+		}
+	}, '');
+	return sqlParams.join(' AND ');
+}
+// Подключаем модуль
+export default function flats(app, pool) {
+	let EasyYandexS3 = pkg.default;
+	// Инициализация
+	let s3 = new EasyYandexS3({
+		auth: {
+			accessKeyId: process.env.YANDEXS3_ACCESS_KEY_ID,
+			secretAccessKey: process.env.YANDEXS3_SECRET_ACCESS_KEY,
+		},
+		Bucket: 'property-images', // например, "my-storage",
+		debug: false, // Дебаг в консоли, потом можете удалить в релизе
+	});
+	// app.use(multer().any());
+
+	app.post('/add_apartment', async (req, res) => {
+		const { flat, floor, m2, price, year, residence, rooms } = req.body;
+		const [scheme, ...photos] = req.files;
+		let dataItemId;
+		const urls = await pool
+			.query('INSERT INTO property_apartments SET ?', {
+				flat,
+				floor,
+				m2,
+				price,
+				year,
+				residence,
+				rooms,
+			})
+			.then((data) => {
+				if (data[0]) {
+					dataItemId = data[0].insertId;
+					const upload = s3.Upload(
+						[
+							{
+								buffer: scheme.buffer,
+								name: scheme.originalname,
+							},
+							...photos.map((item) => {
+								return {
+									buffer: item.buffer,
+									name: item.originalname,
+								};
+							}),
+						],
+						`/Flats/${data[0].insertId}/`,
+					);
+					res.json({ message: 'apartment successfully added' });
+					return upload;
+				}
+				res.json({ message: 'error' });
+				return;
+			});
+		urls.map((item, index) => {
+			pool.query('INSERT INTO property_photos SET ?', {
+				parentId: dataItemId,
+				url: item.Location,
+				type: index === 0 ? 'scheme' : 'photo',
+			});
 		});
 	});
+	app.get('/apartments/get_all', async function (req, res) {
 
+		const sqlString =
+			convertFiltersToSQL(req.query).length > 0
+				? `(SELECT * FROM property_apartments WHERE ${convertFiltersToSQL(
+						req.query,
+				  )}) AS property_apartments`
+				: 'property_apartments';
+		const [apartments] = await pool.query(
+			`SELECT * FROM ${sqlString}
+				INNER JOIN property_photos ON property_apartments.id = property_photos.parentId 
+				WHERE property_photos.type = "scheme"`,
+		);
+
+		const apartmentsIds = apartments.map((item) => item.parentId).join(', ');
+		const apartmentCondition = apartmentsIds.length > 0 ?  `WHERE parentId IN (${apartmentsIds})` : '';
+		const [photos] = await pool.query(
+			`SELECT * FROM property_photos ${apartmentCondition}`,
+		);
+
+		const modifiedApartments = apartments.map((item) => {
+			return {
+				...item,
+				photos: photos.filter(
+					(photo) => photo.parentId === item.parentId && photo.type === 'photo',
+				),
+			};
+		});
+		return res.json(modifiedApartments);
+	});
 }
